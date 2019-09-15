b2014e29ddf1f8e704caedb0029a8ec8c8708ff3
hodai <hoquandai@gmail.com>
2019-09-15 14:04:13 +0700

    update calculateWinner

--- src/Caro.js
+++ src/Caro.js
@@ -159,37 +159,93 @@ function calculateWinner(squares, w, h) {
     let bottom_right = [i, i + (w + 1), i + (w + 1)*2, i + (w + 1)*3, i + (w + 1)*4];
     
     if (!left.some(value => value < 0 || value > (w * h - 1))) {
+      let l_obs = left[0] - 5;
+      let r_obs = left[0] + 1;
+      if (l_obs >= 0 && r_obs < w*h) {
+        left.push(l_obs, r_obs)
+      }
+
       lines.push(left)
     }
     if (!right.some(value => value < 0 || value > (w * h - 1))) {
+      let l_obs = right[0] - 1;
+      let r_obs = right[0] + 5;
+      if (l_obs >= 0 && r_obs < w*h) {
+        right.push(l_obs, r_obs)
+      }
+
       lines.push(right)
     }
     if (!top.some(value => value < 0 || value > (w * h - 1))) {
+      let t_obs = top[0] - 5*w;
+      let b_obs = top[0] + w;
+      if (t_obs >= 0 && b_obs < w*h) {
+        top.push(t_obs, b_obs)
+      }
+
       lines.push(top)
     }
     if (!bottom.some(value => value < 0 || value > (w * h - 1))) {
+      let t_obs = bottom[0] - w;
+      let b_obs = bottom[0] + 5*w;
+      if (t_obs >= 0 && b_obs < w*h) {
+        bottom.push(t_obs, b_obs)
+      }
+
       lines.push(bottom)
     }
     if (!top_left.some(value => value < 0 || value > (w * h - 1))) {
+      let tl_obs = top_left[0] - (w + 1)*5;
+      let br_obs = top_left[0] + (w + 1);
+      if (tl_obs >= 0 && br_obs < w*h) {
+        top_left.push(tl_obs, br_obs)
+      }
+
       lines.push(top_left)
     }
     if (!top_right.some(value => value < 0 || value > (w * h - 1))) {
+      let tr_obs = top_right[0] - (w - 1)*5;
+      let bl_obs = top_right[0] + (w - 1);
+      if (tr_obs >= 0 && bl_obs < w*h) {
+        top_right.push(tr_obs, bl_obs)
+      }
+
       lines.push(top_right)
     }
     if (!bottom_left.some(value => value < 0 || value > (w * h - 1))) {
+      let tr_obs = bottom_left[0] - (w - 1);
+      let bl_obs = bottom_left[0] + (w - 1)*5;
+      if (tr_obs >= 0 && bl_obs < w*h) {
+        bottom_left.push(tr_obs, bl_obs)
+      }
+
       lines.push(bottom_left)
     }
     if (!bottom_right.some(value => value < 0 || value > (w * h - 1))) {
+      let tl_obs = bottom_right[0] - (w + 1);
+      let br_obs = bottom_right[0] + (w + 1)*5;
+      if (tl_obs >= 0 && br_obs < w*h) {
+        bottom_right.push(tl_obs, br_obs)
+      }
+
       lines.push(bottom_right)
     }
-    
   }
 
 console.log(lines)
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c, d, e] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
-      return squares[a];
+    if (lines[i].length === 5) {
+      const [a, b, c, d, e] = lines[i];
+   
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
+        return squares[a];
+      }
+    } else if (lines[i].length === 7) {
+      const [a, b, c, d, e, f, g] = lines[i];
+   
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e] && squares[f] !== squares[g]) {
+        return squares[a];
+      }
     }
   }
   return null;
